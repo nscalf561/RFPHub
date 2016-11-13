7884cefd3d141dc4aff98ea50f131f509deb6992
nscalf561 <scalf.nicholas@gmail.com>
2016-11-13 14:27:08 -0800

    Add users controller + model

--- RFPHub/config/routes.rb
+++ RFPHub/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'application/index'
+
+
+
+  # root 'application#index' #set root to a login page?
+
 end
