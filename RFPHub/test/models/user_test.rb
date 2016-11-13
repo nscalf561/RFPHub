1a68628e05261d64847d6b08b03b91760817f0a9
nscalf561 <scalf.nicholas@gmail.com>
2016-11-13 14:31:39 -0800

    Add basic user model tests

--- RFPHub/test/models/user_test.rb
+++ RFPHub/test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Test User", email: "testuser@test.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name sholud be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+
 end
