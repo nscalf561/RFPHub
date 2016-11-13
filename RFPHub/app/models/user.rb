abeb35cc26348a0380b3a950b4681c4db595fde9
nscalf561 <scalf.nicholas@gmail.com>
2016-11-13 15:20:28 -0800

    Update user model & start making auth

--- RFPHub/app/models/user.rb
+++ RFPHub/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presense: true
+
+  has_secure_password
+
 end
--- RFPHub/db/schema.rb
+++ RFPHub/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113222532) do
+ActiveRecord::Schema.define(version: 20161113225716) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "emails"
+    t.string   "email"
     t.string   "password_digest"
     t.string   "organization"
     t.string   "admin"
