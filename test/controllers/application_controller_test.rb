b51556c2eacc50c8c6d84df0710c3cf21053d9d9
nscalf561 <scalf.nicholas@gmail.com>
2016-11-15 15:11:38 -0800

    Comment out application, sessions, and user controller testing

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get application_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get application_index_url
+  #   assert_response :success
+  # end
 
 end
